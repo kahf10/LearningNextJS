88e604e7d87a1dd1645d4715994ba3977eb2fca2
Kahf Hussain <kahfnhussain@gmail.com>
2025-02-25 14:09:30 -0500

    Parellel routes and Unmatched routes

---
+++ src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,5 @@
+import { Card } from "@/components/card";
+
+export default function UserAnalaytics() {
+  return <Card>User Analaytics</Card>;
+}
--- src/app/error-wrapper.tsx
+++ src/app/error-wrapper.tsx
@@ -15,24 +15,12 @@ const ErrorSimulator = ({
   const [error, setError] = useState(false);
 
   if (error) throw new Error(message);
-
-  return (
-    <button
-      title="Simulate an error"
-      className="bg-red-950 text-red-500 rounded p-1 leading-none font-semibold text-start"
-      onClick={() => setError(true)}
-    >
-      Simulate Error
-    </button>
-  );
 };
 
 export const ErrorWrapper = ({ children }: WrapperProps) => {
   return (
     <div className="flex flex-col rounded-lg mt-8 relative p-4 border-gray-300">
-      <div className="absolute top-10 left-4 -translate-y-1/2">
-        <ErrorSimulator message="Simulated error in root layout"></ErrorSimulator>
-      </div>
+      <div className="absolute top-10 left-4 -translate-y-1/2"></div>
       {children}
     </div>
   );
