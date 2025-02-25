38ce8df23d0fa1fa6690b0731cbcfaaa50fe63da
Kahf Hussain <kahfnhussain@gmail.com>
2025-02-25 15:04:09 -0500

    Understanding Intercepting routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -7,13 +7,16 @@ export default function ComplexDashboardLayout({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = true;
+  return isLoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -24,5 +27,7 @@ export default function ComplexDashboardLayout({
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
---
+++ src/app/f1/(..)f3/page.tsx
@@ -0,0 +1,3 @@
+export default function Intercepted32() {
+  return <h1> (..) Intercepted F3 page </h1>;
+}
